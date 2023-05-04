32dea4da8c5df5cdbefffec66acbf13fb6686b72
TZ-Antonio-Rebelo <tz.ajsr@gmail.com>
2023-05-05 00:45:02 +0100

    User login

--- src/models/LoginModel.js
+++ src/models/LoginModel.js
@@ -16,45 +16,58 @@ class Login {
         this.user = null;
     }
 
-    async register() {
+    async login() {
         this.validate();
         if(this.errors.length > 0) return;
+        this.user = await LoginModel.findOne({ email: this.body.email });
+
+        if(!this.user) {
+            this.errors.push('User does not exist.');
+            return;
+        }
+
+        if(!bcryptjs.compareSync(this.body.password, this.user.password)) {
+            this.errors.push('Invalid password.');
+            this.user = null;
+            return;
+        }
+    }
+
+    async register() {
+        this.validate();
+        if (this.errors.length > 0) return;
 
         await this.userExists();
 
-        if(this.errors.length > 0) return;
+        if (this.errors.length > 0) return;
 
         const salt = bcryptjs.genSaltSync();
         this.body.password = bcryptjs.hashSync(this.body.password, salt);
 
-        try{
-            this.user = await LoginModel.create(this.body);
-        } catch(e) {
-            console.log(e);
-        }
+        this.user = await LoginModel.create(this.body);
     }
 
     async userExists() {
-        const user = await LoginModel.findOne({ email: this.body.email });
+        this.user = await LoginModel.findOne({ email: this.body.email });
 
-        if(user) this.errors.push('User already exists.');
+        if (this.user) this.errors.push('User already exists.');
     }
 
     validate() {
         this.cleanUp();
         // Validação
         // O e-mail precisa ser válido
-        if(!validator.isEmail(this.body.email)) this.errors.push('Invalid e-mail');
+        if (!validator.isEmail(this.body.email)) this.errors.push('Invalid e-mail');
 
         // A senha precisa ter entre 3 e 50
-        if(this.body.password.length < 3 || this.body.password.length > 50) {
+        if (this.body.password.length < 3 || this.body.password.length > 50) {
             this.errors.push('Password must be between 3 and 50 characters.');
         }
     }
 
     cleanUp() {
-        for(const key in this.body) {
-            if(typeof this.body[key] !== 'string') {
+        for (const key in this.body) {
+            if (typeof this.body[key] !== 'string') {
                 this.body[key] = '';
             }
         }
